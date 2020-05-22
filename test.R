78f9a2db5d3f51d6a80028dc3d90bd06795b1df2
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-05-22 14:42:07 +0200

    added tests

---
+++ test.R
@@ -0,0 +1,34 @@
+library(testthat)
+
+source("R/exercise.R")
+
+test_that("ATM works correctly", {
+	r <- atm(50)
+	expect_length(r, 7)
+	expect_named(r)
+	expect_equal(names(r), c("500", "200", "100", "50", "20", "10", "5"))
+	expect_equal(r, list(`500` = 0, `200` = 0, `100` = 0, `50` = 1, `20` = 0, `10` = 0, 
+    `5` = 0))
+	
+	r <- atm(90)
+	expect_length(r, 7)
+	expect_named(r)
+	expect_equal(names(r), c("500", "200", "100", "50", "20", "10", "5"))
+	expect_equal(r, list(`500` = 0, `200` = 0, `100` = 0, `50` = 1, `20` = 2, `10` = 0, 
+    `5` = 0))
+	
+	r <- atm(2520)
+	expect_length(r, 7)
+	expect_named(r)
+	expect_equal(names(r), c("500", "200", "100", "50", "20", "10", "5"))
+	expect_equal(r, list(`500` = 5, `200` = 0, `100` = 0, `50` = 0, `20` = 1, `10` = 0, 
+    `5` = 0))
+})
+
+test_that("Observed agreement is calculated correctly", {
+	expect_equal(po(devmatrix), 0.378)
+})
+
+test_that("Expected agreement is calculated correctly", {
+	expect_equal(pe(devmatrix), 0.213)
+})
