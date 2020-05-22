156eb611d7c7924326880836c6868a10dbfa2a6e
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2020-05-22 14:48:20 +0200

    Revert "fine-tuning tests"
    This reverts commit b291e3a4fc6bfbc4dce37f6823b0fcbdf7eeb04f.

--- R/exercise.R
+++ R/exercise.R
@@ -1,19 +1,2 @@
 devmatrix <- matrix(data= c(0,0,0,0,14,0,2,6,4,2,0,0,3,5,6,0,3,9,2,0,2,2,8,1,1,7,7,0,0,0,3,2,6,3,0,2,5,3,2,2,6,5,2,1,0,0,2,2,3,7), ncol=5, byrow=TRUE)
 
-atm <- function(x) {
-	ret <- list()
-	for (v in c(500,200,100,50,20,10,5)) {
-		num <- x %/% v
-		x <- x - (v*num)
-		ret[[as.character(v)]] <- num
-	}
-	ret
-}
-
-po <- function(m) {
-	mean(apply(m, 1, function(x) { sum(x*(x-1))/(sum(x)*(sum(x)-1)) }))
-}
-
-pe <- function(m, N=nrow(m), n=sum(m[1,])) {
-	sum(apply(m, 2, function(x) {sum(x)/(N*n)})**2)
-}
